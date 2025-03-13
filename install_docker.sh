8aefb94794972faa5360753b599fc200303800c3
Mike Lu <mcumaple@hotmail.com>
2025-03-13 11:07:14 +0800

    Update urls

--- install_docker.sh
+++ install_docker.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
 
+
+# Fixed settings
+red='\e[41m'
+green='\e[32m'
+yellow='\e[93m'
+nc='\e[0m'
+
+
+
+# Ensure the user is running the script as root
+if [ "$EUID" -ne 0 ]; then 
+    echo -e "${yellow}Please run as root to start the installation.${nc}"
+    exit 1
+fi
+
 # Check Internet connectivity
 CheckNetwork() {
-	[[ $(nslookup "google.com" > /dev/null) ]] && echo -e "❌ No Internet connection! Check your network and retry.\n" && exit || :
+	[[ $(nslookup "google.com" > /dev/null) ]] && echo -e "${red}No Internet connection! Check your network and retry.${nc}\n" && exit || :
 }
+CheckNetwork
+
 
 # Identify Linux distro
 [[ -f /usr/bin/apt ]] && PKG=apt || PKG=dnf
@@ -31,7 +48,7 @@ case $PKG in
 	sudo apt update
 	sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 	sudo usermod -aG docker $USER && sudo chmod a+rw /var/run/docker.sock
-	echo -e "\e[32mDone!\e[0m"
+	echo -e "${green}Done!${nc}"
 	# Install the NVIDIA Container Toolkit on Ubuntu
 	echo
 	echo "╭───────────────────────────────────────╮"
@@ -39,7 +56,7 @@ case $PKG in
 	echo "│                                       │"
 	echo "╰───────────────────────────────────────╯"
 	echo
-	if [[ $(lsmod | grep nvidia > /dev/null) ]]; then
+	if [[ ! $(lsmod | grep nvidia > /dev/null) ]]; then
 		curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 		curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 		sudo apt update
@@ -49,7 +66,7 @@ case $PKG in
 		sudo systemctl restart docker
 		# Test GPU integration
 		sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
-		echo -e "\e[32mDone!\e[0m"
+		echo -e "${green}Done!${nc}"
 	else
 		echo -e "NVIDIA driver is not installed. Stop configuring with GPU" && exit 1
 	fi
@@ -63,13 +80,21 @@ case $PKG in
 	echo "│                                       │"
 	echo "╰───────────────────────────────────────╯"
 	echo
-	containerd_url='https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/containerd.io-1.7.25-3.1.el9.x86_64.rpm'
-	docker_buildx_url='https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-buildx-plugin-0.21.1-1.el9.x86_64.rpm'
-	docker_ce_url='hhttps://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-ce-28.0.1-1.el9.x86_64.rpm' #   
-	docker_ce_cli_url='https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-ce-cli-28.0.1-1.el9.x86_64.rpm' #   
-	docker_ce_rootless_url='https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-ce-rootless-extras-28.0.1-1.el9.x86_64.rpm' 
-	docker_compose_url='https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-compose-plugin-2.33.1-1.el9.x86_64.rpm'
-	wget $containerd_url $docker_buildx_url $docker_ce_url $docker_ce_cli_url $docker_ce_rootless_url $docker_compose_url
+    urls=(
+      'https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/containerd.io-1.7.25-3.1.el9.x86_64.rpm'
+      'https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-buildx-plugin-0.21.1-1.el9.x86_64.rpm'
+      'https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-ce-28.0.1-1.el9.x86_64.rpm'
+      'https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-ce-cli-28.0.1-1.el9.x86_64.rpm'
+      'https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-ce-rootless-extras-28.0.1-1.el9.x86_64.rpm'
+      'https://download.docker.com/linux/rhel/9/x86_64/stable/Packages/docker-compose-plugin-2.33.1-1.el9.x86_64.rpm'
+    )
+    for url in "${urls[@]}"; do
+      filename=$(basename "$url")
+      if [ ! -f "$filename" ]; then
+        wget "$url"
+      fi
+    done
+    
 	sudo subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
 	sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 	sudo dnf install -y pass
@@ -79,7 +104,7 @@ case $PKG in
 	sudo dnf install -y ./*.rpm -y && sudo rm -f ./*.rpm 
 	sudo systemctl enable docker
 	sudo systemctl start docker
-	echo -e "\e[32mDone!\e[0m"
+	echo -e "${green}Done!${nc}"
 	# Install the NVIDIA Container Toolkit on RHEL9
 	echo
 	echo "╭───────────────────────────────────────╮"
@@ -87,7 +112,7 @@ case $PKG in
 	echo "│                                       │"
 	echo "╰───────────────────────────────────────╯"
 	echo
-	if [[ $(lsmod | grep nvidia > /dev/null) ]]; then
+	if [[ ! $(lsmod | grep nvidia > /dev/null) ]]; then
 		curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 		sudo dnf install -y nvidia-container-toolkit
 		# Configure Docker to use Nvidia driver
@@ -95,7 +120,7 @@ case $PKG in
 		sudo systemctl restart docker
 		# Test GPU integration
 		sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
-		echo -e "\e[32mDone!\e[0m"
+		echo -e "${green}Done!${nc}"
 	else
 		echo -e "NVIDIA driver is not installed. Stop configuring with GPU" && exit 1
 	fi
